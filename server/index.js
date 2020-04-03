6dceee18d6a5ca6a674565af34eabfbc8eec9833
cferdinand <chrisferdinand07@gmail.com>
2020-04-03 18:56:02 -0400

    change index.js to serve static correctly

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const compression = require("compression");
-const path = require("path");
 const app = express();
+const path = require("path");
 const port = 3000;
 
 app.use(compression());
 
 app.use(
-  "/products/:productid",
   express.static(path.join(__dirname, "/../client/dist"), {
     maxAge: 1,
     index: false
   })
 );
+
+app.use("/products/:productid", express.static("dist"));
 
 app.get("/", (req, res) => {
   res.redirect("/products/4");
